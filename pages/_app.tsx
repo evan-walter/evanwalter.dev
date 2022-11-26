e9d53f47ac10d964d976efefc2344e22c10737df
Evan Walter <evan.walter.software@gmail.com>
2022-11-26 02:08:21 -0500

    style: home text spacing; refactor: head to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from 'components/Navbar'
@@ -5,6 +6,15 @@ import Navbar from 'components/Navbar'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Evan Walter | Full Stack Engineer</title>
+        <meta
+          name='description'
+          content='React | Next.js | Node.js | TypeScript | GraphQL'
+        />
+        <link rel='icon' href='/favicon.svg' />
+      </Head>
+
       <div className='container relative mx-auto'>
         <Navbar />
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
-import Head from 'next/head'
 import HomeComponent from 'components/HomeComponent'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Evan Walter | Full Stack Engineer</title>
-        <meta
-          name='description'
-          content='React | Next.js | Node.js | TypeScript | GraphQL'
-        />
-        <link rel='icon' href='/favicon.svg' />
-      </Head>
-
       <main>
         <HomeComponent />
       </main>
