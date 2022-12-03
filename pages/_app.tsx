f31811376480c9a1ce9cb840ea9da38a486d4790
Evan Walter <evan.walter.software@gmail.com>
2022-12-02 16:18:14 -0800

    style: see all page contents on mobile landscape

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,13 +15,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel='icon' href='/favicon.svg' />
       </Head>
 
-      <div className='container relative mx-auto h-screen min-h-min px-4'>
+      <div className='container relative mx-auto h-screen min-h-[35rem] px-4'>
         <Navbar />
         <Component {...pageProps} />
       </div>
 
       <footer className='flex flex-col items-center justify-center gap-y-10 bg-zinc-800 py-10'>
-        <button title='Building software on the web since 2019'>❤️‍🔥</button>
+        <div className='container mx-auto flex flex-col items-center justify-center px-4'>
+          <button title='Building software on the web since 2019'>❤️‍🔥</button>
+        </div>
       </footer>
     </>
   )
