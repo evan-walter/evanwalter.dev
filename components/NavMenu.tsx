aae1e89d5885b1aefa45a554335e1604e9cadd2c
Evan Walter <evan.walter.software@gmail.com>
2022-12-22 11:54:10 -0800

    format: autoformat classes
    fix: pnpm appears to be buggy. revert to npm.

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -34,8 +34,8 @@ export default function NavMenu() {
   }, [isMenuOpen, isMobileScreen])
 
   return (
-    <nav className='container absolute top-0 left-0 right-0 bg-white py-8 dark:bg-zinc-900 max-sm:px-6 max-w-2xl'>
-      <div className='relative flex w-full max-sm:flex-col items-start justify-between gap-y-3 gap-x-8 rounded-lg sm:items-center sm:justify-center'>
+    <nav className='container absolute top-0 left-0 right-0 max-w-2xl bg-white py-8 dark:bg-zinc-900 max-sm:px-6'>
+      <div className='flex w-full items-start justify-between gap-y-3 gap-x-8 rounded-lg max-sm:flex-col sm:items-center sm:justify-center'>
         <MobileMenuButtons
           isMenuOpen={isMenuOpen}
           setIsMenuOpen={setIsMenuOpen}
@@ -92,33 +92,39 @@ export default function NavMenu() {
   )
 }
 
-function ThemeButton() {
-  const { resolvedTheme, setTheme } = useTheme()
+interface MobileMenuButtonsProps {
+  isMenuOpen: Boolean
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function MobileMenuButtons({
+  isMenuOpen,
+  setIsMenuOpen,
+}: MobileMenuButtonsProps) {
+  function handleClick() {
+    setIsMenuOpen((s) => !s)
+  }
 
   return (
-    <button
-      className='text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-white'
-      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      onClick={handleClick}
+      className='block h-6 w-6 sm:hidden'
     >
-      <svg
-        className='w-6 h-6'
-        fill='none'
-        stroke='currentColor'
-        viewBox='0 0 24 24'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth={2}
-          d={
-            resolvedTheme === 'dark'
-              ? 'M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
-              : 'M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
-          }
-        />
-      </svg>
-    </button>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d={
+          isMenuOpen
+            ? 'M6 18L18 6M6 6l12 12'
+            : 'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+        }
+      />
+    </svg>
   )
 }
 
@@ -172,36 +178,26 @@ function NavItem({
   )
 }
 
-interface MobileMenuButtonsProps {
-  isMenuOpen: Boolean
-  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-function MobileMenuButtons({
-  isMenuOpen,
-  setIsMenuOpen,
-}: MobileMenuButtonsProps) {
-  function handleClick() {
-    setIsMenuOpen((s) => !s)
-  }
+function ThemeButton() {
+  const { resolvedTheme, setTheme } = useTheme()
 
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6 text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-white'
       fill='none'
-      viewBox='0 0 24 24'
-      strokeWidth={1.5}
       stroke='currentColor'
-      onClick={handleClick}
-      className='block h-6 w-6 sm:hidden'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
       <path
         strokeLinecap='round'
         strokeLinejoin='round'
+        strokeWidth={2}
         d={
-          isMenuOpen
-            ? 'M6 18L18 6M6 6l12 12'
-            : 'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+          resolvedTheme === 'dark'
+            ? 'M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+            : 'M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
         }
       />
     </svg>
