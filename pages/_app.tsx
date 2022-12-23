da815a748d80329b49c05fa0c0d12928bc7f8126
Evan Walter <evan.walter.software@gmail.com>
2022-12-23 12:35:06 -0800

    wip: set up context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import Head from 'next/head'
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { createContext, useContext } from 'react'
 import NavMenu from 'components/NavMenu'
 
+const ThemeContext = createContext('')
+
 export default function App({ Component, pageProps }: AppProps) {
+  const textLinkColors = useContext(ThemeContext)
+
   return (
     <>
       <Head>
@@ -17,16 +22,20 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <ThemeProvider attribute='class'>
-        <div className='container relative h-screen min-h-[45rem] max-w-2xl sm:min-h-[35rem]'>
-          <NavMenu />
-          <Component {...pageProps} />
-        </div>
-
-        <footer className='flex flex-col items-center justify-center gap-y-10 bg-zinc-100 py-10 dark:bg-zinc-800'>
-          <div className='container flex flex-col items-center justify-center'>
-            <button title='Building software on the web since 2019'>❤️‍🔥</button>
+        <ThemeContext.Provider value='textLinkColors'>
+          <div className='container relative h-screen min-h-[45rem] max-w-2xl sm:min-h-[35rem]'>
+            <NavMenu />
+            <Component {...pageProps} />
           </div>
-        </footer>
+
+          <footer className='flex flex-col items-center justify-center gap-y-10 bg-zinc-100 py-10 dark:bg-zinc-800'>
+            <div className='container flex flex-col items-center justify-center'>
+              <button title='Building software on the web since 2019'>
+                ❤️‍🔥
+              </button>
+            </div>
+          </footer>
+        </ThemeContext.Provider>
       </ThemeProvider>
     </>
   )
