c68b7923dd7cb80fee3f15c3d78883038937bead
Evan Walter <evan.walter.software@gmail.com>
2022-12-23 13:00:58 -0800

    wip: refactor: context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,12 @@ import { ThemeProvider } from 'next-themes'
 import { createContext, useContext } from 'react'
 import NavMenu from 'components/NavMenu'
 
-const ThemeContext = createContext('')
+const TextLinkContext = createContext(
+  'text-zinc-500 hover:text-black dark:text-zinc-400 dark:hover:text-white'
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  const textLinkColors = useContext(ThemeContext)
+  const textLinkColors = useContext(TextLinkContext)
 
   return (
     <>
@@ -22,20 +24,18 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <ThemeProvider attribute='class'>
-        <ThemeContext.Provider value='textLinkColors'>
-          <div className='container relative h-screen min-h-[45rem] max-w-2xl sm:min-h-[35rem]'>
+        <div className='container relative h-screen min-h-[45rem] max-w-2xl sm:min-h-[35rem]'>
+          <TextLinkContext.Provider value={textLinkColors}>
             <NavMenu />
             <Component {...pageProps} />
-          </div>
+          </TextLinkContext.Provider>
+        </div>
 
-          <footer className='flex flex-col items-center justify-center gap-y-10 bg-zinc-100 py-10 dark:bg-zinc-800'>
-            <div className='container flex flex-col items-center justify-center'>
-              <button title='Building software on the web since 2019'>
-                ❤️‍🔥
-              </button>
-            </div>
-          </footer>
-        </ThemeContext.Provider>
+        <footer className='flex flex-col items-center justify-center gap-y-10 bg-zinc-100 py-10 dark:bg-zinc-800'>
+          <div className='container flex flex-col items-center justify-center'>
+            <button title='Building software on the web since 2019'>❤️‍🔥</button>
+          </div>
+        </footer>
       </ThemeProvider>
     </>
   )
