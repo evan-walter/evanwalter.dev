14bd8ccc959b97a9a20017a4eeaae99720374fed
Evan Walter <evan.walter.software@gmail.com>
2023-01-31 15:10:56 -0800

    Clear unnecessary text classes styling

--- components/PodcastAppearance.tsx
+++ components/PodcastAppearance.tsx
@@ -5,7 +5,7 @@ export default function PodcastAppearance() {
 
   return (
     <div className='rounded-lg bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 p-0.5'>
-      <p className='rounded-lg bg-white px-5 py-4 text-xl text-black dark:bg-zinc-900 dark:text-white max-md:text-center'>
+      <p className='rounded-lg bg-white px-5 py-4 text-xl dark:bg-zinc-900 max-md:text-center'>
         I am excited to announce that I had the privilege of appearing as a
         guest on{' '}
         <a
