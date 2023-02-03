c514009c5855224c477433be1f61ace97e32a676
Evan Walter <evan.walter.software@gmail.com>
2023-02-02 18:49:10 -0800

    Update about page spacing style

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,10 +3,10 @@ import Contact from 'components/Contact'
 
 export default function podcast() {
   return (
-    <>
-      <h1 className='mb-3 text-3xl font-bold sm:text-4xl'>About</h1>
+    <div className='flex flex-col gap-y-2'>
+      <h1 className='mb-1 text-3xl font-bold sm:text-4xl'>About</h1>
       <About />
       <Contact />
-    </>
+    </div>
   )
 }
