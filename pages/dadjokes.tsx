e14ab29228467e0f1a6f67f45d42a029648ff4d9
Evan Walter <evan.walter.software@gmail.com>
2023-02-04 16:28:10 -0800

    Improve dad jokes responsive styles

--- pages/dadjokes.tsx
+++ pages/dadjokes.tsx
@@ -29,54 +29,52 @@ export default function App() {
 
   return (
     <>
-      <div className='px-10'>
-        <div className='mx-auto mb-10 flex max-w-2xl flex-col items-center justify-between border-b border-gray-300 pb-10 sm:flex-row'>
-          <Btn addClassNames='bg-indigo-500' onClick={fetchData}>
-            Get a New Random Joke
-          </Btn>
-          <a
-            href='https://karljoke.herokuapp.com/'
-            target='_blank'
-            rel='noreferrer noopener'
-            className='mt-5 text-blue-500 underline hover:text-blue-600 sm:mt-0'
-          >
-            View API Docs
-          </a>
-        </div>
-        <div className='mx-auto flex max-w-2xl flex-col'>
-          {loading ? (
-            <Status addClassNames='text-gray-500'>Loading your joke...</Status>
-          ) : null}
-          {success ? (
-            <>
-              <div className='relative my-10'>
-                <div className='leading-0 absolute top-0 left-0 -z-10 font-serif text-9xl text-gray-200'>
-                  “
-                </div>
-                <JokeText addClassNames='sm:text-left px-20'>{setup}</JokeText>
-              </div>
-              <Btn
-                addClassNames='bg-pink-500 mx-auto'
-                onClick={() => setShowPunchline(!showPunchline)}
-              >
-                {showPunchline ? 'Hide Punchline' : 'Show Punchline'}
-              </Btn>
-            </>
-          ) : null}
-          {showPunchline ? (
+      <div className='mx-auto flex max-w-2xl flex-col items-center justify-between border-b border-gray-300 pb-10 sm:flex-row'>
+        <Btn addClassNames='bg-indigo-500' onClick={fetchData}>
+          Get a New Random Joke
+        </Btn>
+        <a
+          href='https://karljoke.herokuapp.com/'
+          target='_blank'
+          rel='noreferrer noopener'
+          className='mt-5 text-blue-500 underline hover:text-blue-600 sm:mt-0'
+        >
+          View API Docs
+        </a>
+      </div>
+      <div className='mx-auto flex max-w-2xl flex-col'>
+        {loading ? (
+          <Status addClassNames='text-gray-500 mt-10'>
+            Loading your joke...
+          </Status>
+        ) : null}
+        {success ? (
+          <>
             <div className='relative my-10'>
-              <div className='leading-0 absolute inset-y-0 right-0 -z-10 font-serif text-9xl text-gray-200'>
-                ”
+              <div className='leading-0 absolute top-0 left-0 -z-10 font-serif text-9xl text-gray-200'>
+                “
               </div>
-              <JokeText addClassNames='sm:text-right px-20'>
-                {punchline}
-              </JokeText>
+              <JokeText addClassNames='sm:text-left'>{setup}</JokeText>
             </div>
-          ) : null}
-          {failure ? (
-            <Status>There was an error loading your joke.</Status>
-          ) : null}
-        </div>
+            <Btn
+              addClassNames='bg-pink-500 mx-auto'
+              onClick={() => setShowPunchline(!showPunchline)}
+            >
+              {showPunchline ? 'Hide Punchline' : 'Show Punchline'}
+            </Btn>
+          </>
+        ) : null}
+        {showPunchline ? (
+          <div className='relative my-10'>
+            <div className='leading-0 absolute inset-y-0 right-0 -z-10 font-serif text-9xl text-gray-200'>
+              ”
+            </div>
+            <JokeText addClassNames='sm:text-right'>{punchline}</JokeText>
+          </div>
+        ) : null}
+        {failure ? (
+          <Status>There was an error loading your joke.</Status>
+        ) : null}
       </div>
     </>
   )
@@ -118,5 +116,11 @@ interface JokeTextProps {
 }
 
 export function JokeText({ addClassNames, children }: JokeTextProps) {
-  return <p className={`${addClassNames} text-center text-2xl`}>{children}</p>
+  return (
+    <p
+      className={`${addClassNames} text-center text-2xl max-[485px]:pt-20 min-[485px]:px-20`}
+    >
+      {children}
+    </p>
+  )
 }
