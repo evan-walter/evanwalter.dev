42230ff899f46e1f4bb7b14788162c79de13ecae
Evan Walter <evan.walter.software@gmail.com>
2023-02-10 14:05:52 -0800

    Add smooth scrolling to top; style
    Style burning heart button text to display absolute for more positioning control

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,84 +5,104 @@ export default function Footer() {
   const navLinkColors = useNavLinkContext()
 
   return (
-    <footer className='flex flex-col items-center justify-center py-10 gap-y-10 bg-zinc-200 dark:bg-zinc-800'>
-      <div className='container flex w-full max-w-2xl items-center justify-between gap-4 max-[375px]:flex-col'>
-        <FooterLinksGroup>
-          <Link href='/' className={navLinkColors}>
-            Home
-          </Link>
-          <Link href='/podcast' className={navLinkColors}>
-            Podcast
-          </Link>
-          <Link href='/works' className={navLinkColors}>
-            Works
-          </Link>
-          <Link href='/about' className={navLinkColors}>
-            About
-          </Link>
-        </FooterLinksGroup>
-        <FooterLinksGroup>
-          <a
-            href='https://quiz-preview.vercel.app/'
-            className={navLinkColors}
-            target='_blank'
-            rel='noreferrer noopener'
+    <footer className='bg-zinc-200 py-10 dark:bg-zinc-800'>
+      <div className='container flex w-full max-w-2xl flex-col gap-y-8'>
+        <Link href='#' className={`${navLinkColors} mx-auto p-1`}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-6 w-6'
           >
-            Quiz App
-          </a>
-          <a
-            href='https://lens-starter-app.vercel.app/'
-            className={navLinkColors}
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            Lens Social App
-          </a>
-          <Link href='/dad-jokes' className={navLinkColors}>
-            Dad Jokes
-          </Link>
-          <a
-            href='/resume.pdf'
-            className={navLinkColors}
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            Resume
-          </a>
-        </FooterLinksGroup>
-        <FooterLinksGroup>
-          <a
-            href='https://github.com/evan-walter'
-            className={navLinkColors}
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            GitHub
-          </a>
-          <a
-            href='https://linkedin.com/in/-evanwalter'
-            className={navLinkColors}
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            LinkedIn
-          </a>
-          <a
-            href='https://twitter.com/_evanwalter_'
-            className={navLinkColors}
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            Twitter
-          </a>
-          <div className='invisible max-[374px]:hidden'>Home</div>
-        </FooterLinksGroup>
-      </div>
-      <div className='flex flex-col justify-center group gap-y-2'>
-        <button title='Building software on the web since 2019'>❤️‍🔥</button>
-        <p className='px-4 py-2 text-center transition duration-700 border border-orange-500 rounded-full opacity-0 group-hover:opacity-100 group-hover:ease-in-out dark:border-yellow-500'>
-          Building software on the web since 2019
-        </p>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M4.5 15.75l7.5-7.5 7.5 7.5'
+            />
+          </svg>
+        </Link>
+
+        <div className='flex items-center justify-between gap-4 max-[375px]:flex-col'>
+          <FooterLinksGroup>
+            <Link href='/' className={navLinkColors}>
+              Home
+            </Link>
+            <Link href='/podcast' className={navLinkColors}>
+              Podcast
+            </Link>
+            <Link href='/works' className={navLinkColors}>
+              Works
+            </Link>
+            <Link href='/about' className={navLinkColors}>
+              About
+            </Link>
+          </FooterLinksGroup>
+          <FooterLinksGroup>
+            <a
+              href='https://quiz-preview.vercel.app/'
+              className={navLinkColors}
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              Quiz App
+            </a>
+            <a
+              href='https://lens-starter-app.vercel.app/'
+              className={navLinkColors}
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              Lens Social App
+            </a>
+            <Link href='/dad-jokes' className={navLinkColors}>
+              Dad Jokes
+            </Link>
+            <a
+              href='/resume.pdf'
+              className={navLinkColors}
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              Resume
+            </a>
+          </FooterLinksGroup>
+          <FooterLinksGroup>
+            <a
+              href='https://github.com/evan-walter'
+              className={navLinkColors}
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              GitHub
+            </a>
+            <a
+              href='https://linkedin.com/in/-evanwalter'
+              className={navLinkColors}
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              LinkedIn
+            </a>
+            <a
+              href='https://twitter.com/_evanwalter_'
+              className={navLinkColors}
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              Twitter
+            </a>
+            <div className='invisible max-[374px]:hidden'>Home</div>
+          </FooterLinksGroup>
+        </div>
+
+        <div className='group mx-auto w-fit pb-10'>
+          <button className='p-1.5' title='Building software on the web since 2019'>❤️‍🔥</button>
+          <p className='mt-2 absolute left-96 rounded-full border border-orange-500 px-4 py-2 text-center opacity-0 transition duration-700 group-hover:opacity-100 group-hover:ease-in-out dark:border-yellow-500'>
+            Building software on the web since 2019
+          </p>
+        </div>
       </div>
     </footer>
   )
