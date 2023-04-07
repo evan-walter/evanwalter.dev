5d7351cc7abc786c6686c5bfa894e94a6433c80c
Evan Walter <evan.walter.software@gmail.com>
2023-04-07 11:01:12 -0700

    Set up GPT redirects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = (phase, { defaultConfig }) => {
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects() {
+      return [
+        {
+          source: '/gpt-3',
+          destination: '/gpt-model',
+          permanent: true,
+        },
+      ]
+    },
+  }
+  return nextConfig
 }
-
-module.exports = nextConfig
