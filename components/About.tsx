c4b69665b42f73de28ce74e8ef9c2e49ac11459e
evan-walter <ewpw5678@gmail.com>
2025-01-25 22:33:39 -0800

    feat: use AP-style title case

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
   return (
     <div className='flex flex-col gap-y-2 text-lg'>
       <p className='first-letter:float-left first-letter:mr-3 first-letter:font-serif first-letter:text-7xl first-letter:font-bold'>
-        I am a Full-Stack Software Engineer. I have a high value for learning
+        I am a Full-stack Software Engineer. I have a high value for learning
         and offering valuable contribution as a team member. I specialize in
         TypeScript, React, and Tailwind CSS. With a background in Electrical
         Engineering, I offer a unique attention to detail and creativity in
